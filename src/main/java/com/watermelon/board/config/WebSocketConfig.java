f7d529c441b5a07e15e428fa528b5ec5834605e2
Waxwell21 <971032710@qq.com>
2022-11-11 16:05:21 +0800

    添加handler类，修改了config文件

--- src/main/java/com/watermelon/board/config/WebSocketConfig.java
+++ src/main/java/com/watermelon/board/config/WebSocketConfig.java
@@ -1,13 +1,29 @@
 package com.watermelon.board.config;
 
-import org.springframework.context.annotation.Bean;
+import com.watermelon.board.handler.HttpAuthHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 
 @Configuration
-public class WebSocketConfig {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Autowired
+    private HttpAuthHandler httpAuthHandler;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(httpAuthHandler, "myWS")
+                .setAllowedOrigins("*");
     }
 }
+
+/*
+添加注解 @EnableWebSocket 表示开启了webocket服务
+registerWebSocketHandlers 这个方法用来注册websocket服务，上面表示将路径 myWS的请求转发到 WebSocketHandler
+etAllowedOrigins("*") **这个是关闭跨域校验，方便本地调试*/
